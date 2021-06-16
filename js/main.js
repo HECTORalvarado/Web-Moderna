318b57c72b3dc97294736226211bef62c8360909
Héctor Alvarado <hectoralexanderalvarado@gmail.com>
2021-06-16 13:13:10 -0600

    Se agrego el menu del sitio web

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let boton = document.getElementById('icono');
+let enlaces = document.getElementById('enlaces');
+let MostrarMenu = false;
+
+
+boton.addEventListener('click', e=>{
+  if (MostrarMenu == false) {
+    enlaces.className = ('enlaces dos');
+    MostrarMenu = true;
+  } else {
+    enlaces.classList.remove('dos');
+    enlaces.className = ('enlaces uno');
+    MostrarMenu = false;
+  }
+})
